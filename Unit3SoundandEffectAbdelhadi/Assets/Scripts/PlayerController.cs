364c72978e9bae22c3e89a723975ed6618197a22
Ibrahim Abdelhadi <727349@student.dusd.net>
2024-04-16 11:38:54 -0700

    game
    made player have animation and jump animation

--- Unit3SoundandEffectAbdelhadi/Assets/Scripts/PlayerController.cs
+++ Unit3SoundandEffectAbdelhadi/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControler : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
@@ -23,11 +26,21 @@ public class PlayerControler : MonoBehaviour
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         isOnGround = true;
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+        }
     }
 }
